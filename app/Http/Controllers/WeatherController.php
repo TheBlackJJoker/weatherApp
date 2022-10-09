0f4a2ff5b1b3066acdbc3c67a1254d5d117b9365
Zbigniew Kwasniewicz <zbigniew.kwasniewicz@gmail.com>
2022-10-09 13:27:49 +0200

    implements contructor promotion

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,17 +3,18 @@
 declare(strict_types=1);
 
 namespace App\Http\Controllers;
+
 use App\Repository\WeatherRepository;
 use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    private WeatherRepository $weatherRepository;
-    public function __construct(WeatherRepository $weatherRepository){
-        $this->weatherRepository = $weatherRepository;
+    public function __construct(private WeatherRepository $weatherRepository)
+    {
     }
 
-    function searchView(): Object{
-        return view('weather', ['content'=>$this->weatherRepository->get()]);
+    function searchView(): Object
+    {
+        return view('weather', ['content' => $this->weatherRepository->get()]);
     }
 }
