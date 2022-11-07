d07355675842f045b42b017ecb296feb0994c208
Zbigniew Kwasniewicz <zbigniew.kwasniewicz@gmail.com>
2022-11-07 21:11:21 +0100

    Do DTO better without fromWttrin - new function in WttrinService

--- app/DTOs/WeatherData.php
+++ app/DTOs/WeatherData.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\DTOs;
 
-use GuzzleHttp\Psr7\Response;
-
 final class WeatherData
 {
     public function __construct(
@@ -16,18 +14,4 @@ final class WeatherData
         public readonly float $latitude,
         public readonly float $longitude,
     ) {}
-
-    public static function fromWttrin(Response $response): self
-    {
-        $data = json_decode($response->getBody()->getContents(), false);
-
-        return new self(
-            intval($data->current_condition[0]->temp_C),
-            $data->current_condition[0]->lang_pl[0]->value,
-            $data->nearest_area[0]->areaName[0]->value,
-            $data->nearest_area[0]->country[0]->value,
-            floatval($data->nearest_area[0]->latitude),
-            floatval($data->nearest_area[0]->longitude),
-        );
-    }
 }
--- app/Services/WttrinService.php
+++ app/Services/WttrinService.php
@@ -9,6 +9,7 @@ use App\Interfaces\Weather;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Response;
 
 class WttrinService implements Weather
 {
@@ -28,6 +29,20 @@ class WttrinService implements Weather
             return throw new Exception("Nie znaleziono stacji meteorologicznej!");
         }
 
-        return WeatherData::fromWttrin($response);
+        return $this->dto($response);
+    }
+
+    private function dto(Response $response)
+    {
+        $data = json_decode($response->getBody()->getContents(), false);
+
+        return new WeatherData(
+            intval($data->current_condition[0]->temp_C),
+            $data->current_condition[0]->lang_pl[0]->value,
+            $data->nearest_area[0]->areaName[0]->value,
+            $data->nearest_area[0]->country[0]->value,
+            floatval($data->nearest_area[0]->latitude),
+            floatval($data->nearest_area[0]->longitude),
+        );
     }
 }
